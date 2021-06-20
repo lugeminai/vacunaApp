119577250da96e895bc761912779a70d8d3a3b37
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-06-20 12:01:38 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ConsultapadronComponent } from './pages/consultapadron/consultapadron.component';
+import { ConsultalocalComponent } from './pages/consultalocal/consultalocal.component';
+import { LocalComponent } from './pages/local/local.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConsultapadronComponent,
+    ConsultalocalComponent,
+    LocalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
