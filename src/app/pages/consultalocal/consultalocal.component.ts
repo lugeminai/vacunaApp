760e4f7fc0d377ce8167c61a6417fe36bd51789a
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-06-20 12:02:01 -0500

    Create consultalocal.component.ts

---
+++ src/app/pages/consultalocal/consultalocal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultalocal',
+  templateUrl: './consultalocal.component.html',
+  styleUrls: ['./consultalocal.component.scss']
+})
+export class ConsultalocalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
