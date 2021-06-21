321fc260341714e861b2b0f4a60a62c77c476075
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-06-21 13:04:42 -0500

    Create padronvacunacion.component.ts

---
+++ src/app/pages/consultapadron/padronvacunacion/padronvacunacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-padronvacunacion',
+  templateUrl: './padronvacunacion.component.html',
+  styleUrls: ['./padronvacunacion.component.scss']
+})
+export class PadronvacunacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
